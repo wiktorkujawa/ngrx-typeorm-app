d1ca874e4a91e21f367b4ecf358efdd1f214e7d7
wiktor <wiktorkujawa1993@gmail.com>
2021-01-27 14:11:32 +0100

    Added custom material theme styles and toggle themes button

--- src/app/modules/public/public-layout/public-layout.component.ts
+++ src/app/modules/public/public-layout/public-layout.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-public-layout',
   templateUrl: './public-layout.component.html',
-  styleUrls: ['./public-layout.component.scss']
+  styleUrls: ['./public-layout.component.scss'],
 })
 export class PublicLayoutComponent {
-
-  isMobile$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
+  isMobile$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.XSmall)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  onSwitchTheme({ checked }: any) {
+    checked
+      ? this.document.body.classList.add('alternate-theme')
+      : this.document.body.classList.remove('alternate-theme');
+  }
 
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 }
