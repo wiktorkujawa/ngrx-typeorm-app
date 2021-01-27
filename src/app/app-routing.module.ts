08b861a9d917a13ddb57459c2670a145f29f1a40
wiktor <wiktorkujawa1993@gmail.com>
2021-01-27 01:59:22 +0100

    Created material module and admin/public layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminLayoutComponent } from './modules/admin/admin-layout/admin-layout.component';
+import { PublicLayoutComponent } from './modules/public/public-layout/public-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: PublicLayoutComponent,
+  // children: [
+  //  {
+  //    path: 'transactions',
+  //    loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule)
+  //  },
+  //  {
+  //    path: '', component: AboutPageComponent
+  //  }
+//  ]
+},
+{
+  path: 'admin',
+  component: AdminLayoutComponent,
+  // children: [
+  //  {
+  //    path: 'transactions',
+  //    loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule)
+  //  },
+  //  {
+  //    path: '', component: AboutPageComponent
+  //  }
+//  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/public/public-layout/public-layout.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-public-layout',
+  templateUrl: './public-layout.component.html',
+  styleUrls: ['./public-layout.component.scss']
+})
+export class PublicLayoutComponent {
+
+  isMobile$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
