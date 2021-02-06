c9e5af8a521e0ebf69ae6716082881087ec70552
wiktor <wiktorkujawa1993@gmail.com>
2021-02-06 23:30:23 +0100

    Innitialized typeorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,72 @@
-import 'reflect-metadata';
-import express from 'express';
-import cors from 'cors';
+import "reflect-metadata";
+import dotenv from "dotenv";
 import path from 'path';
+dotenv.config({ path: path.join(__dirname,'../.env' )});
+import {createConnection} from "typeorm";
+import express from "express";
+import bodyParser from "body-parser";
+import {Request, Response} from "express";
+import {Routes} from "./routes";
+import {User} from "./entity/User";
 
-const app = express();
+createConnection({
+  type: "mongodb",
+  url: process.env.mongoURI,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  synchronize: true,
+  logging: false,
+  entities: [
+     __dirname+"/entity/**/*.js"
+  ],
+  migrations: [
+    __dirname+"/migration/**/*.js"
+  ],
+  subscribers: [
+    __dirname+"/subscriber/**/*.js"
+  ],
+  cli: {
+     entitiesDir: __dirname+"/entity",
+     migrationsDir: __dirname+"/migration",
+     subscribersDir: __dirname+"/subscriber"
+  }
+}).then(async connection => {
 
-app.use(
-  cors({
-    origin: "http://localhost:4200",
-    credentials: true
-  })
-);
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
 
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next);
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
 
-app.use(express.static(path.join(__dirname, '../dist')));
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+        });
+    });
 
-app.get('*', (_, res) => { 
-    res.sendFile(path.join(__dirname, '../dist/index.html')) 
-}); 
+    // setup express app here
+    // ...
 
-const PORT = process.env.PORT || 4000; 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    // start express server
+    app.listen(3000);
+
+    // insert new users for test
+    await connection.manager.save(connection.manager.create(User, {
+        firstName: "Timber",
+        lastName: "Saw",
+        age: 27
+    }));
+    await connection.manager.save(connection.manager.create(User, {
+        firstName: "Phantom",
+        lastName: "Assassin",
+        age: 24
+    }));
+
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+
+}).catch(error => console.log(error));
